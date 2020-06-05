d97acf139c9020fe2cc2bf0873b92e7cbff90cdf
Jolien De Letter <joliendeletter@gmail.com>
2020-06-05 10:55:38 +0200

    Today and welcome message

---
+++ Controllers/TodayController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieWeb.Controllers
+{
+    public class TodayController : Controller
+    {
+      
+        public IActionResult FullDate()
+        {
+            return View();
+        }
+
+    }
+}
